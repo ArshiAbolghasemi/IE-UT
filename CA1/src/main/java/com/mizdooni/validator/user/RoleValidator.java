cec394b51ead52c53291c268abb8292b594cf70b
a.abolghasemi <a.abolghasemi@digikala.com>
2024-02-26 02:38:44 +0330

    feat: adding role validator
    this validator validate role format and valud that is valid user role or not

---
+++ CA1/src/main/java/com/mizdooni/validator/user/RoleValidator.java
@@ -0,0 +1,28 @@
+package com.mizdooni.validator.user;
+
+import com.mizdooni.entity.UserEntity;
+
+import java.util.Arrays;
+
+public class RoleValidator {
+
+    private final String value;
+
+    public RoleValidator(String value) {
+        this.value = value;
+    }
+
+    public void validate() throws AssertionError {
+        this.validateFormat();
+        this.checkValidUserRole();
+    }
+
+    private void validateFormat() throws AssertionError {
+        assert this.value != null && !this.value.isEmpty() : "role should not be empty!";
+    }
+
+    private void checkValidUserRole() throws AssertionError {
+        assert Arrays.asList(UserEntity.ALL_ROLE).contains(this.value) :
+                String.format("role should be in %s", String.join(", ", UserEntity.ALL_ROLE))
+    }
+}

f5dd01cc90119504fed51b9fa4d5a9787da40894
a.abolghasemi <a.abolghasemi@digikala.com>
2024-02-25 20:22:51 +0330

    feat: adding required DTOs for add user cmd
    adding AddUserCommandDTO and AddressDTO for representing json argument in adduser command

---
+++ CA1/src/main/java/com/mizdooni/lib/dto/AddressDTO.java
@@ -0,0 +1,21 @@
+package com.mizdooni.lib.dto;
+
+public class AddressDTO {
+
+    private String country;
+
+    private String city;
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCountry() { return this.country; }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCity() { return this.city; }
+
+}
---
+++ CA1/src/main/java/com/mizdooni/lib/dto/user/AddUserCommandDTO.java
@@ -0,0 +1,57 @@
+package com.mizdooni.lib.dto.user;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonSetter;
+import com.mizdooni.lib.dto.AddressDTO;
+import java.util.Map;
+
+public class AddUserCommandDTO {
+    private String username;
+
+    private String password;
+
+    private String email;
+
+    private String role;
+
+    private AddressDTO address;
+
+    @JsonSetter("username")
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() { return this.username; }
+
+    @JsonSetter("password")
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() { return this.password; }
+
+    @JsonSetter("role")
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getRole() { return this.role; }
+
+    @JsonSetter("email")
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getEmail() { return this.email; }
+
+    @JsonProperty("address")
+    public void setAddress(Map<String,Object> address) {
+        AddressDTO addressDTO = new AddressDTO();
+        addressDTO.setCountry((String) address.get("country"));
+        addressDTO.setCity((String) address.get("city"));
+        this.address = addressDTO;
+    }
+
+    public AddressDTO getAddress() { return this.address; }
+
+}

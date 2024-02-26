1655d621d8de98b68903a99baf158fd2445b52a9
a.abolghasemi <a.abolghasemi@digikala.com>
2024-02-26 18:08:49 +0330

    feat: adding AddNewTableCommandDTO
    this class represent Add table command args

---
+++ CA1/src/main/java/com/mizdooni/lib/dto/request/restaurant/table/AddNewTableCommandDTO.java
@@ -0,0 +1,34 @@
+package com.mizdooni.lib.dto.request.restaurant.table;
+
+import com.fasterxml.jackson.annotation.JsonSetter;
+
+public class AddNewTableCommandDTO {
+
+    private int tableNumber;
+
+    private String restaurantName;
+
+    private String managerUsername;
+
+    private int seatNumbers;
+
+    @JsonSetter("tableNumber")
+    public void setTableNumber(int tableNumber) { this.tableNumber = tableNumber; }
+
+    public int getTableNumber() { return this.tableNumber; }
+
+    @JsonSetter("restaurantName")
+    public void setRestaurantName(String restaurantName) { this.restaurantName = restaurantName; }
+
+    public String getRestaurantName() { return this.restaurantName; }
+
+    @JsonSetter("managerUsername")
+    public void setManagerUsername(String managerUsername) { this.managerUsername = managerUsername; }
+
+    public String getManagerUsername() { return this.managerUsername; }
+
+    public void setSeatNumbers(int seatNumbers) { this.seatNumbers = seatNumbers; }
+
+    public int getSeatNumbers() { return this.seatNumbers; }
+
+}

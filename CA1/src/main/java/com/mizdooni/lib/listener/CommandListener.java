d4a15277cf8b667ad49a2b62e3f82c2d6b6d99c7
a.abolghasemi <a.abolghasemi@digikala.com>
2024-02-26 04:39:57 +0330

    feat: adding command listener
    this command having map from command name to command handler class and also handling each command typed by user

---
+++ CA1/src/main/java/com/mizdooni/lib/listener/CommandListener.java
@@ -0,0 +1,78 @@
+package com.mizdooni.lib.listener;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mizdooni.form.user.AddNewUserForm;
+import com.mizdooni.lib.command.Command;
+import com.mizdooni.lib.command.Form;
+import com.mizdooni.lib.dto.response.CommandResponseDTO;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommandListener {
+
+    private final Map<String, String> commands = new HashMap<>();
+
+    public CommandListener() {
+        this.init();
+    }
+
+    private void init() {
+        this.register("addUser", AddNewUserForm.class.getName());
+    }
+
+    private CommandListener register(String commandName, String commandClass) throws RuntimeException, AssertionError {
+        try {
+            assert Command.class.isAssignableFrom(Class.forName(commandClass)) :
+                    String.format("%s should implement %s", commandClass, Command.class.getName());
+            this.commands.put(commandName, commandClass);
+
+            return this;
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private Command find(String command) throws Exception {
+        if (!this.commands.containsKey(command)) {
+            throw new Exception("command doesn't exists!");
+        }
+
+        return (Command) Class.forName(this.commands.get(command)).getDeclaredConstructor().newInstance();
+    }
+
+    private void successResponse(CommandResponseDTO commandResponseDTO) {
+        this.response(commandResponseDTO, true);
+    }
+
+    private void errorResponse(CommandResponseDTO commandResponseDTO) {
+        this.response(commandResponseDTO, false);
+    }
+
+    private void response(CommandResponseDTO commandResponseDTO, boolean success) {
+        commandResponseDTO.put("success", success);
+        try {
+            System.out.println(new ObjectMapper().writeValueAsString(commandResponseDTO));
+        } catch (JsonProcessingException e) {
+            System.out.println("something goes wrong!");
+        }
+    }
+
+    public void run(String cmd, String[] args) {
+        try {
+            Command command = this.find(cmd);
+
+            if (command instanceof Form) {
+                ((Form) command).execute(args);
+            }
+
+            CommandResponseDTO commandResponseDTO = command.getData(args);
+            this.successResponse(commandResponseDTO);
+        } catch (Throwable e) {
+            CommandResponseDTO commandResponseDTO = new CommandResponseDTO();
+            commandResponseDTO.put("data", e.getMessage());
+            this.errorResponse(commandResponseDTO);
+        }
+    }
+}

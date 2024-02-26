e8c5cff5ccfc95ff29240bf552112d76f5a2fd07
a.abolghasemi <a.abolghasemi@digikala.com>
2024-02-26 14:58:05 +0330

    feat: register AddNewRestaurantForm to command listener

--- CA1/src/main/java/com/mizdooni/lib/listener/CommandListener.java
+++ CA1/src/main/java/com/mizdooni/lib/listener/CommandListener.java
@@ -2,6 +2,7 @@ package com.mizdooni.lib.listener;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mizdooni.form.restaurant.AddNewRestaurantForm;
 import com.mizdooni.form.user.AddNewUserForm;
 import com.mizdooni.lib.command.Command;
 import com.mizdooni.lib.command.Form;
@@ -19,7 +20,9 @@ public class CommandListener {
     }
 
     private void init() {
-        this.register("addUser", AddNewUserForm.class.getName());
+        this
+                .register("addUser", AddNewUserForm.class.getName())
+                .register("addRestaurant", AddNewRestaurantForm.class.getName());
     }
 
     private CommandListener register(String commandName, String commandClass) throws RuntimeException, AssertionError {

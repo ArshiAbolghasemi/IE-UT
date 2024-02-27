02c8285ff002adb127ec6f3fe4f316029e1d0fa7
a.abolghasemi <a.abolghasemi@digikala.com>
2024-02-27 22:46:17 +0330

    feat: initiate ReserrveTableForTest class

---
+++ CA1/src/test/java/com/mizdooni/form/ReserveTableFormTest.java
@@ -0,0 +1,100 @@
+package com.mizdooni.form;
+
+import com.mizdooni.entity.*;
+import com.mizdooni.form.restaurant.table.ReserveTableForm;
+import com.mizdooni.repository.*;
+import com.mizdooni.service.PasswordService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.Assert.assertEquals;
+
+public class ReserveTableFormTest {
+
+    private ReserveTableForm reserveTableForm;
+
+    private TableReservationRepository tableReservationRepository;
+
+    private UserRepository userRepository;
+
+    private RestaurantRepository restaurantRepository;
+
+    private AddressRepository addressRepository;
+
+    private RestaurantAddressRepository restaurantAddressRepository;
+
+    private TableRepository tableRepository;
+
+    @Before
+    public void setup() {
+        this.reserveTableForm = new ReserveTableForm();
+        this.tableReservationRepository = TableReservationRepository.getInstance();
+        this.userRepository = UserRepository.getInstance();
+        this.restaurantRepository = RestaurantRepository.getInstance();
+        this.tableRepository = TableRepository.getInstance();
+        this.addressRepository = AddressRepository.getInstance();
+        this.restaurantAddressRepository = RestaurantAddressRepository.getInstance();
+        this.tableRepository = TableRepository.getInstance();
+        this.createRequiredEntities();
+    }
+
+    private void createRequiredEntities() {
+        AddressEntity addressEntity = new AddressEntity()
+                .setCity("city")
+                .setCountry("country");
+        this.addressRepository.persist(addressEntity);
+
+        UserEntity client = new UserEntity()
+                .setUsername("client")
+                .setRole(UserEntity.ROLE_CLIENT)
+                .setPassword(PasswordService.getInstance().hash("1234"))
+                .setMail("client@example.com")
+                .setAddressId(addressEntity.getId());
+        this.userRepository.persist(client);
+
+        UserEntity manager = new UserEntity()
+                .setUsername(UserEntity.ROLE_MANAGER)
+                .setPassword(PasswordService.getInstance().hash("4567"))
+                .setMail("manager@example.com")
+                .setAddressId(addressEntity.getId());
+        this.userRepository.persist(manager);
+
+        RestaurantAddressEntity restaurantAddressEntity = new RestaurantAddressEntity()
+                .setCity("test_city")
+                .setCountry("test_country")
+                .setStreet("test_street");
+        this.restaurantAddressRepository.persist(restaurantAddressEntity);
+
+        RestaurantEntity restaurantEntity = new RestaurantEntity()
+                .setName("restaurant")
+                .setType("iranian")
+                .setDescription("description")
+                .setRestaurantAddressId(restaurantAddressEntity.getId())
+                .setStartTime(LocalTime.parse("09:00"))
+                .setEndTime(LocalTime.parse("23:00"));
+        this.restaurantRepository.persist(restaurantEntity);
+
+        TableEntity tableEntity = new TableEntity()
+                .setTableNumber(1)
+                .setRestaurantId(restaurantEntity.getId())
+                .setSeatsNumber(2);
+        this.tableRepository.persist(tableEntity);
+    }
+
+    @After
+    public void tearDown() {
+        this.reserveTableForm = null;
+        this.tableReservationRepository = null;
+        this.userRepository = null;
+        this.restaurantRepository = null;
+        this.tableRepository = null;
+        this.restaurantAddressRepository = null;
+        this.addressRepository = null;
+    }
+
+}
